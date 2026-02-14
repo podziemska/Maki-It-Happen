ed55b20c678069b73694820150d7bcbd8a22b679
Ratisss <rafal.kontakt.twitch@gmail.com>
2026-02-15 00:11:25 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maki_it_happen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Graj_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. Tworzymy instancję nowego okna z kuchnią
+            GameWindow oknoGry = new GameWindow();
+
+            // 2. Pokazujemy nowe okno
+            oknoGry.Show();
+
+            // 3. Zamykamy obecne menu, żeby nie wisiało w tle
+            this.Close();
+
+            //I cyk pyk jesteśmy w kuchni!
+        }
+
+        private void Wyjdz_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); // The Walking Dead
+        }
+    }
+}
