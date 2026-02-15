7cfefb4075a0819efd8bdcc7e7e95098df960547
Ratisss <rafal.kontakt.twitch@gmail.com>
2026-02-15 21:02:25 +0100

    chyba zadziała

--- GameWindow.xaml.cs
+++ GameWindow.xaml.cs
@@ -11,30 +11,35 @@ namespace Maki_it_happen
 
         private void AddRice_Click(object sender, RoutedEventArgs e)
         {
+            usun.Content = "ryż";
             // TODO: Zmniejsz licznik ryżu o 1
             // TODO: Dodaj grafikę ryżu na talerz 
         }
 
         private void AddFish_Click(object sender, RoutedEventArgs e)
         {
+            usun.Content = "ryba";
             // TODO: Zmniejsz licznik ryby o 1
             // TODO: Dodaj grafikę ryby na talerz
         }
 
         private void AddCucumber_Click(object sender, RoutedEventArgs e)
         {
+            usun.Content = "ogórek";
             // TODO: Zmniejsz licznik ogórka o 1
             // TODO: Dodaj grafikę ogórka na talerz
         }
 
         private void AddNori_Click(object sender, RoutedEventArgs e)
         {
+            usun.Content = "nori";
             // TODO: Zmniejsz licznik nori o 1
             // TODO: Dodaj grafikę nori na talerz
         }
 
         private void Serve_Click(object sender, RoutedEventArgs e)
         {
+            usun.Content = "serwuj";
             // TODO: Sprawdź czy składniki na talerzu pasują do zamówienia
             // TODO: Jeśli tak -> dodaj kasę i wyczyść talerz
         }
--- ShopWindow.xaml.cs
+++ ShopWindow.xaml.cs
@@ -1,27 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Maki_it_happen
 {
-    /// <summary>
-    /// Logika interakcji dla klasy ShopWindow.xaml
-    /// </summary>
     public partial class ShopWindow : Window
     {
         public ShopWindow()
         {
             InitializeComponent();
+        }
+
+        private void BuyRice_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: if (Kasa >= 20) { Kasa -= 20; Ryz += 10; }
+        }
+
+        private void BuyFish_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: if (Kasa >= 50) { Kasa -= 50; Losos += 5; }
+        }
+
+        private void BuyNori_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: if (Kasa >= 15) { Kasa -= 15; Nori += 20; }
+        }
+
+        private void BackToKitchen_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // Zamyka tylko sklep i wraca do otwartej kuchni
         }
     }
 }
