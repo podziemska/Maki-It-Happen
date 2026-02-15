8b183a48b47a6b3d32405f957e72ec8770b30a51
osozanska <sozanskaoliwka@gmail.com>
2026-02-15 19:24:57 +0100

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maki_it_happen
+{
+    internal class Product
+    {
+       
+        public string ?Nazwa { get; set; }
+        public double Cena { get; set; }
+        public int Id { get; set; }
+
+        public Product(string Nazwa, double Cena, int Id) {
+            this.Nazwa = Nazwa;
+            this.Cena = Cena;
+            this.Id = Id;
+        }
+
+        public virtual string AboutProduct() {
+            return $"Produkt o id {Id} i nazwie {Nazwa} kosztuje {Cena}";
+        }
+
+        public void Cost() {
+            if (Cena <= 0)
+            {
+                Console.WriteLine("Cena musi byc dodatnia.");
+            }
+            
+        }
+
+    }
+}
---
+++ Sushi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maki_it_happen
+{
+    internal class  Sushi : Product
+    {
+        public int Ilosc { get; set; }
+        public string ?Skladniki { get; set; }
+        public int PoziomOstrosci { get; set; }
+        public bool CzyWegetarianskie { get; set; }
+
+        public Sushi(string Nazwa, double Cena, int Id, int Ilosc, string Skladniki, int PoziomOstrosci, bool CzyWegetarianskie) : base(Nazwa, Cena, Id) 
+        {
+
+            this.Ilosc = Ilosc;
+            this.Skladniki = Skladniki;
+            this.PoziomOstrosci = PoziomOstrosci;
+            this.CzyWegetarianskie = CzyWegetarianskie;
+
+        }
+
+        public override string AboutProduct()
+        {
+            string Odpowiedz;
+            if (CzyWegetarianskie == true)
+            {
+                Odpowiedz = "jest";
+            }
+            else {
+                Odpowiedz = "nie jest";
+            }
+                return $"Produkt o id {Id} i nazwie {Nazwa} kosztuje {Cena}. Posiada {Ilosc} kawalkow o {Skladniki} skladnikach. Poziom ostrosci sushi wynosi {PoziomOstrosci}, a sushi {Odpowiedz} wegetarianskie";
+            
+        }
+    }
+}
