43f431372520b1cae1f277f34700ab7c3e463c7f
podziemska <podziemskablanka@gmail.com>
2026-02-19 01:52:53 +0100

    Add files via upload

---
+++ Zamowienie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maki_it_happen
+{
+    internal class Zamowienie
+    {
+        public Product Produkt { get; set; }
+        public int Ilosc { get; set; }
+        public decimal CenaZamowienia { get; set; }
+
+        public Zamowienie(Product produkt, int ilosc, decimal cena)
+        {
+            Produkt = produkt;
+            Ilosc = ilosc;
+            CenaZamowienia = cena;
+
+        }
+
+        public void ObliczCeneZamowienia()
+        {
+            CenaZamowienia = (decimal)(Produkt.Cena * Ilosc);
+        }
+    }
+}
