c59213bf3a6485568f4f731242ea53df0cac5473
osozanska <sozanskaoliwka@gmail.com>
2026-02-21 15:04:16 +0100

    Add item counters for rice, fish, cucumber, and nori

--- GameWindow.xaml.cs
+++ GameWindow.xaml.cs
@@ -4,6 +4,11 @@ namespace Maki_it_happen
 {
     public partial class GameWindow : Window
     {
+        int IloscRyzu = 10;
+        int IloscRyby = 5;
+        int IloscOgorka = 8;
+        int IloscNori = 12;
+
         public GameWindow()
         {
             InitializeComponent();
@@ -12,28 +17,64 @@ namespace Maki_it_happen
         private void AddRice_Click(object sender, RoutedEventArgs e)
         {
             usun.Content = "ryż";
-            // TODO: Zmniejsz licznik ryżu o 1
+            
+            
+            if (IloscRyzu >= 1)
+            {
+                IloscRyzu--;
+                RiceCountLabel.Text = $"Szt: {IloscRyzu}";
+            }
+            else {
+                RiceCountLabel.Text = "Ryż się skończył! Idz do sklepu!";
+            }
+
             // TODO: Dodaj grafikę ryżu na talerz 
         }
 
         private void AddFish_Click(object sender, RoutedEventArgs e)
         {
             usun.Content = "ryba";
-            // TODO: Zmniejsz licznik ryby o 1
+          
+            if (IloscRyby >= 1)
+            {
+                IloscRyby--;
+                FishCountLabel.Text = $"Szt: {IloscRyby}";
+            }
+            else
+            {
+                FishCountLabel.Text = "Łosoś się skończył! Idz do sklepu!";
+            }
             // TODO: Dodaj grafikę ryby na talerz
         }
 
         private void AddCucumber_Click(object sender, RoutedEventArgs e)
         {
             usun.Content = "ogórek";
-            // TODO: Zmniejsz licznik ogórka o 1
+          
+            if (IloscOgorka >= 1)
+            {
+                IloscOgorka--;
+                CucumberCountLabel.Text = $"Szt: {IloscOgorka}";
+            }
+            else
+            {
+                CucumberCountLabel.Text = "Ogórek się skończył! Idz do sklepu!";
+            }
             // TODO: Dodaj grafikę ogórka na talerz
         }
 
         private void AddNori_Click(object sender, RoutedEventArgs e)
         {
             usun.Content = "nori";
-            // TODO: Zmniejsz licznik nori o 1
+            if (IloscNori >= 1)
+            {
+                IloscNori--;
+                NoriCountLabel.Text = $"Szt: {IloscNori}";
+            }
+            else
+            {
+                NoriCountLabel.Text = "Nori się skończyło! Idz do sklepu!";
+            }
             // TODO: Dodaj grafikę nori na talerz
         }
 
