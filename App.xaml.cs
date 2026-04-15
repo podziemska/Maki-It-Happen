6b18d70569a9a8a0e8edb473643b512190ce4f7a
osozanska <sozanskaoliwka@gmail.com>
2026-04-15 17:38:06 +0200

    Play music on application startup
    Override OnStartup to play music on application start.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +9,12 @@ namespace Maki_it_happen
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MusicPlayer.Play("pack://application:,,,/muzyka.mp3");
+        }
     }
 
 }
